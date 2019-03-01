4e3ea18db9a87722e279dc3f2b2ff2d42f02495d
Gin-APTX4869 <live100086@gmail.com>
2019-03-01 12:10:05 +0800

    Updates

---
+++ precache-manifest.f7f6fc0b2749ed0d7149b11303afaecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cabb7d40c71a9afc59e",
+    "url": "/static/css/main.4d2447f3.chunk.css"
+  },
+  {
+    "revision": "9cabb7d40c71a9afc59e",
+    "url": "/static/js/main.9cabb7d4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe0ee76b5c24b16976ca4a87db4d73a7",
+    "url": "/index.html"
+  }
+];
